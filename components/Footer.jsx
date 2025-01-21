cc99cdef7533ff84c84d7c719dac396212c440a6
esaytar <usovaalena01@gmail.com>
2025-01-21 22:17:56 +0400

    fixes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -14,25 +14,29 @@ export default function Footer() {
                     <div className="flex gap-5 items-center self-start md:order-none justify-center">
                         <LogoTSPK styles='fill-white'/>
                     </div>
-                    <nav className="flex w-full justify-between flex-col md:flex-row gap-[1.42rem] md:gap-0">
-                        <div className="flex flex-col gap-[0.71rem] md:gap-5 text-white">
-                            <p className='text-[1.14rem] md:text-[1rem]'>Сведения об образовательной организации</p>
-                            <DataCollegeMenu main='text-[0.85rem] lg:text-[0.75rem] gap-2.5' liW='!w-[15rem]'/>
-                        </div>
-                        <div className="flex flex-col gap-[0.71rem] md:gap-5 text-white">
-                            <p className='text-[1.14rem] md:text-[1rem]'>Колледж</p>
-                            <CollegeMenu main='text-[0.75rem] gap-2.5 flex-col'/>
-                        </div>
-                        <div className="flex flex-col gap-[0.71rem] md:gap-5 text-white">
-                            <p className='text-[1.14rem] md:text-[1rem]'><a href="">БПОО</a></p>
-                            <BPOOMenu main='text-[0.75rem] gap-2.5' liW='!w-[16rem]'/>
-                            <p className='text-[1.14rem] lg:text-[1rem]'><Link href='/dem-exam'>Демонстрационный экзамен</Link></p>
-                            <p className='text-[1.14rem] lg:text-[1rem]'><Link href='/contacts'>Контакты</Link></p>
-                            <div className='flex gap-5 items-center self-center lg:self-start'>
-                                <Vk styles='w-[32px] h-[32px]'/>
-                                <Telegram styles='w-[32px] h-[32px]'/> 
-                            </div>
-                        </div>
+                    <nav className="w-full">
+                        <ul className='flex w-full justify-between flex-col md:flex-row gap-[1.42rem] md:gap-0'>
+                            <li className="flex flex-col gap-[0.71rem] md:gap-5 text-white">
+                                <p className='text-[1.14rem] md:text-[1rem]'>Сведения об образовательной организации</p>
+                                <DataCollegeMenu main='text-[0.85rem] lg:text-[0.75rem] gap-2.5' liW='!w-[15rem]'/>
+                            </li>
+                            <li className="flex flex-col gap-[0.71rem] md:gap-5 text-white">
+                                <p className='text-[1.14rem] md:text-[1rem]'>Колледж</p>
+                                <CollegeMenu main='text-[0.85rem] lg:text-[0.75rem] gap-2.5 flex-col'/>
+                            </li>
+                            <li className="flex flex-col gap-[0.71rem] md:gap-5 text-white">
+                                <p className='text-[1.14rem] md:text-[1rem]'><a href="">БПОО</a></p>
+                                <BPOOMenu main='text-[0.85rem] lg:text-[0.75rem] gap-2.5' liW='!w-[16rem]'/>
+                                <ul className='flex flex-col gap-[0.71rem] md:gap-5'>
+                                    <li className='text-[1.14rem] lg:text-[1rem]'><Link href='/dem-exam'>Демонстрационный экзамен</Link></li>
+                                    <li className='text-[1.14rem] lg:text-[1rem]'><Link href='/contacts'>Контакты</Link></li>
+                                </ul>
+                                <div className='flex gap-5 items-center self-center lg:self-start'>
+                                    <Vk styles='w-[32px] h-[32px]'/>
+                                    <Telegram styles='w-[32px] h-[32px]'/> 
+                                </div>
+                            </li>
+                        </ul>
                     </nav>
                 </div>
                 <p className='text-white text-center text-[0.85rem] md:text-[1rem]'>&copy; {new Date().getFullYear()} Государственное автономное профессиональное образовательное учреждение Самарской области &quot;Тольяттинский социально-педагогический колледж&quot;</p>
