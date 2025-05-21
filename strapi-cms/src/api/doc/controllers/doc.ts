9d2a405636cfd75405eae42cbb8aeb67abcb17f8
Alyona <131011857+esaytar@users.noreply.github.com>
2025-05-21 19:09:26 +0400

    test

--- strapi-cms/src/api/doc/controllers/doc.ts
+++
@@ -1,7 +0,0 @@
-/**
- * doc controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::doc.doc');
--- strapi-cms/src/api/doc/services/doc.ts
+++
@@ -1,7 +0,0 @@
-/**
- * doc service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::doc.doc');
