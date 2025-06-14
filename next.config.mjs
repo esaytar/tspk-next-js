6166c70734e659816f11e9b81c4556508638a20f
Alyona <131011857+esaytar@users.noreply.github.com>
2025-06-14 19:00:50 +0400

    edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
     webpack(config) {
         config.module.rules.push({
           test: /\.(mp4|webm|ogg)$/i,
