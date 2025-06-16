fad404543836efbac4138b8cfb3365de1286d41e
Alyona <131011857+esaytar@users.noreply.github.com>
2025-06-16 11:59:22 +0400

    build news

--- app/api/vk-news/route.js
+++ app/api/vk-news/route.js
@@ -5,7 +5,6 @@ export async function GET() {
     const response = await fetch(`https://api.vk.com/method/wall.get?owner_id=-${process.env.VK_GROUP_ID}&count=100&access_token=${process.env.VK_SERVICE_TOKEN}&v=5.131`);
     const data = await response.json();
 
-    // Возвращаем в вашем формате
     return NextResponse.json({
       response: {
         items: data.response?.items || []
