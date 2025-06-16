9c66331dae9bf3304598212fd02a9e88003f2493
Alyona <131011857+esaytar@users.noreply.github.com>
2025-06-16 22:38:11 +0400

    edited marquee animation

--- components/HeadBlock/HeadBlock.jsx
+++ components/HeadBlock/HeadBlock.jsx
@@ -19,7 +19,7 @@ export default function HeadBlock() {
                         transition-all duration-700 ease-[cubic-bezier(0.2,0,0,1)]`}>
                         <div className={`flex flex-col gap-5 ${raleway.className} `}>
                             <p className='text-base lg:text-18 '>Государственное автономное профессиональное образовательное <br /> учреждение Самарской области</p>
-                            <p className='uppercase text-[2.14rem] leading-[120%] lg:text-[3rem] lg:leading-[3.52rem] font-bold'>Тольяттинский <br /> социально-педагогический колледж</p>
+                            <p className='uppercase text-[2.14rem] leading-[120%] lg:text-[3rem] lg:leading-[3.52rem] font-bold'>Тольяттинский <br /> социально-педагогический <br /> колледж</p>
                         </div>
                         
                         <div className='flex flex-col lg:flex-row gap-5 lg:gap-[1.88rem] items-start leading-[1.32rem]'>
@@ -34,50 +34,24 @@ export default function HeadBlock() {
                         </div>
                     </div>
                 </div>
-                <div className='overflow-hidden '>
-                    <div className={`${raleway.className} ${styles.marquee} flex absolute bottom-0 items-center p-5 whitespace-nowrap`}>
-                        <div className='inline-flex items-center gap-2.5 mx-2.5'>
-                            <svg className='inline-block' width="30" height="28" viewBox="0 0 30 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M14.6822 -0.00978088L15.0239 6.46366C15.2329 10.4241 18.4794 13.6003 22.4664 13.745L29.1735 13.9885L22.474 14.2321C18.4914 14.3768 15.3433 17.553 15.257 21.5134L15.1158 27.9869L14.7742 21.5134C14.5652 17.553 11.3187 14.3768 7.33163 14.2321L0.62452 13.9885L7.32409 13.745C11.3066 13.6003 14.4548 10.4241 14.5411 6.46366L14.6822 -0.00978088Z" fill="white"/>
-                            </svg>
-                            <p className='uppercase text-white text-[1.5rem] inline-block'>Думай иначе, будь первым</p>
+                <div className='w-full absolute bottom-0'>
+                    <div className={`${raleway.className} relative overflow-hidden w-full h-[80px]`}>
+                        <div className="flex absolute bottom-0 left-0 py-5">
+                            <div className={`${styles.marqueeContainer} flex items-center gap-2.5`}>
+                                {[...Array(12)].map((_, i) => (
+                                    <div key={i} className="flex items-center shrink-0">
+                                        <svg className='inline-block' width="30" height="28" viewBox="0 0 30 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M14.6822 -0.00978088L15.0239 6.46366C15.2329 10.4241 18.4794 13.6003 22.4664 13.745L29.1735 13.9885L22.474 14.2321C18.4914 14.3768 15.3433 17.553 15.257 21.5134L15.1158 27.9869L14.7742 21.5134C14.5652 17.553 11.3187 14.3768 7.33163 14.2321L0.62452 13.9885L7.32409 13.745C11.3066 13.6003 14.4548 10.4241 14.5411 6.46366L14.6822 -0.00978088Z" fill="white"/>
+                                        </svg>
+                                        <p className="uppercase text-white text-[1.5rem] ml-2.5 leading-[100%]">
+                                            Думай иначе, будь первым
+                                        </p>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                        <div className='inline-flex items-center gap-2.5 mx-2.5'>
-                            <svg className='inline-block' width="30" height="28" viewBox="0 0 30 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M14.6822 -0.00978088L15.0239 6.46366C15.2329 10.4241 18.4794 13.6003 22.4664 13.745L29.1735 13.9885L22.474 14.2321C18.4914 14.3768 15.3433 17.553 15.257 21.5134L15.1158 27.9869L14.7742 21.5134C14.5652 17.553 11.3187 14.3768 7.33163 14.2321L0.62452 13.9885L7.32409 13.745C11.3066 13.6003 14.4548 10.4241 14.5411 6.46366L14.6822 -0.00978088Z" fill="white"/>
-                            </svg>
-                            <p className='uppercase text-white text-[1.5rem] inline-block'>Думай иначе, будь первым</p>
-                        </div>
-                        <div className='inline-flex items-center gap-2.5 mx-2.5'>
-                            <svg className='inline-block' width="30" height="28" viewBox="0 0 30 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M14.6822 -0.00978088L15.0239 6.46366C15.2329 10.4241 18.4794 13.6003 22.4664 13.745L29.1735 13.9885L22.474 14.2321C18.4914 14.3768 15.3433 17.553 15.257 21.5134L15.1158 27.9869L14.7742 21.5134C14.5652 17.553 11.3187 14.3768 7.33163 14.2321L0.62452 13.9885L7.32409 13.745C11.3066 13.6003 14.4548 10.4241 14.5411 6.46366L14.6822 -0.00978088Z" fill="white"/>
-                            </svg>
-                            <p className='uppercase text-white text-[1.5rem] inline-block'>Думай иначе, будь первым</p>
-                        </div>
-                        <div className='inline-flex items-center gap-2.5 mx-2.5'>
-                            <svg className='inline-block' width="30" height="28" viewBox="0 0 30 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M14.6822 -0.00978088L15.0239 6.46366C15.2329 10.4241 18.4794 13.6003 22.4664 13.745L29.1735 13.9885L22.474 14.2321C18.4914 14.3768 15.3433 17.553 15.257 21.5134L15.1158 27.9869L14.7742 21.5134C14.5652 17.553 11.3187 14.3768 7.33163 14.2321L0.62452 13.9885L7.32409 13.745C11.3066 13.6003 14.4548 10.4241 14.5411 6.46366L14.6822 -0.00978088Z" fill="white"/>
-                            </svg>
-                            <p className='uppercase text-white text-[1.5rem] inline-block'>Думай иначе, будь первым</p>
-                        </div>
-                        <div className='inline-flex items-center gap-2.5 mx-2.5'>
-                            <svg className='inline-block' width="30" height="28" viewBox="0 0 30 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M14.6822 -0.00978088L15.0239 6.46366C15.2329 10.4241 18.4794 13.6003 22.4664 13.745L29.1735 13.9885L22.474 14.2321C18.4914 14.3768 15.3433 17.553 15.257 21.5134L15.1158 27.9869L14.7742 21.5134C14.5652 17.553 11.3187 14.3768 7.33163 14.2321L0.62452 13.9885L7.32409 13.745C11.3066 13.6003 14.4548 10.4241 14.5411 6.46366L14.6822 -0.00978088Z" fill="white"/>
-                            </svg>
-                            <p className='uppercase text-white text-[1.5rem] inline-block'>Думай иначе, будь первым</p>
-                        </div>
-                        <div className='inline-flex items-center gap-2.5 mx-2.5'>
-                            <svg className='inline-block' width="30" height="28" viewBox="0 0 30 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M14.6822 -0.00978088L15.0239 6.46366C15.2329 10.4241 18.4794 13.6003 22.4664 13.745L29.1735 13.9885L22.474 14.2321C18.4914 14.3768 15.3433 17.553 15.257 21.5134L15.1158 27.9869L14.7742 21.5134C14.5652 17.553 11.3187 14.3768 7.33163 14.2321L0.62452 13.9885L7.32409 13.745C11.3066 13.6003 14.4548 10.4241 14.5411 6.46366L14.6822 -0.00978088Z" fill="white"/>
-                            </svg>
-                            <p className='uppercase text-white text-[1.5rem] inline-block'>Думай иначе, будь первым</p>
-                        </div>
-                        
-                        
-                        
                     </div>
                 </div>
-                
             </div>
         </div>
     )
